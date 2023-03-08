639a56f459f8b053198377557baa4d7acf42aba7
Lionel <rldlab84@gmail.com>
2023-03-08 03:24:45 -0300

    added javascripts

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+document.getElementById('contactinformationbuttonshow').addEventListener('click',function()
+{
+    document.getElementById('contactinformationtable').style.display='block';
+});
+
+document.getElementById('contactinformationbuttonhide').addEventListener('click',function()
+{
+    document.getElementById('contactinformationtable').style.display='none';
+});
+
